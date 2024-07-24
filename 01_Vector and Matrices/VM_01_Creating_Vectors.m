0f3f516c59b1af23c3b826e5ac532de32bffa55f
Anna Sánchez <asanchezba@gmail.com>
2024-07-24 12:35:08 +0200

    Add files via upload

---
+++ 01_Vector and Matrices/VM_01_Creating_Vectors.m
@@ -0,0 +1,14 @@
+%Creating vectors
+clc; clear;
+
+% Row vector
+A = [1 5 7 9]
+
+% Column vector
+B = [1; 5; 7; 9]
+
+% Row vector with increments
+C = 1:2:12
+
+% Row vector with decrements
+D = 12:-2:1
